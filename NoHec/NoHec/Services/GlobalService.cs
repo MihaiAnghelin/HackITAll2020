9e7ff7447beb0382af60e29824185786219b5bb0
Mihai <mihai.anghelin@gmail.com>
2020-12-06 02:57:37 +0200

    Added Chart

--- NoHec/NoHec/Services/GlobalService.cs
+++ NoHec/NoHec/Services/GlobalService.cs
@@ -7,6 +7,8 @@ namespace NoHec.Services
     public interface IGlobalService
     {
         Task Register(string email, string password, string firstName, string lastName);
+        Task<List<double>> GetChardData(string symbol);
+
     }
     public class GlobalService : IGlobalService
     {
@@ -20,6 +22,11 @@ namespace NoHec.Services
         public async Task Register(string email, string password, string firstName, string lastName)
         {
             await _httpService.Post<RegisterModel>("/api/auth/register", new { email, password, firstName, lastName });
+        }
+
+        public async Task<List<double>> GetChardData(string symbol)
+        {
+            return await _httpService.Get<List<double>>($"api/dashboard/getGraphData?symbol={symbol}");
         }
     }
 
